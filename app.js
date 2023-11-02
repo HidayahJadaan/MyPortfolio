b0ef000fd60adcb2df1c32c7c50e85290670116e
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-11-02 18:14:39 +0300

    Finishing 90%, without responsive

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", window.scroll > 0 )
+    header.classList.toggle("sticky",  window.scrollY > 0 )
 });
